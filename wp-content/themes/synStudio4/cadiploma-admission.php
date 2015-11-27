2941680e080768190ccb218e20453fe96fa34f4c
Ash Priom <sa.priom@gmail.com>
2015-11-27 14:39:23 -0500

    Sign up Form
    new form

--- wp-content/themes/synStudio4/cadiploma-admission.php
+++ wp-content/themes/synStudio4/cadiploma-admission.php
@@ -1038,7 +1038,7 @@ Template Name: Concept Art Diploma - Admission
             <h1>Admission Application Form</h1>
             <br />
             <br />
-                          <!-- Begin MailChimp Signup Form -->
+            <!-- Begin MailChimp Signup Form 
           <link href="//cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet" type="text/css">
           <style type="text/css">
             #mc_embed_signup{clear:left; font:14px Gotham,Arial,sans-serif; }
@@ -1086,14 +1086,71 @@ Template Name: Concept Art Diploma - Admission
         <div id="mce-responses" class="clear">
           <div class="response" id="mce-error-response" style="display:none"></div>
           <div class="response" id="mce-success-response" style="display:none"></div>
-        </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+        </div>     real people should not fill this in and expect good things - do not remove this or risk form bot signups
           <div style="position: absolute; left: -5000px;"><input type="text" name="b_23c298529438530e62af0db46_e8c037fe10" tabindex="-1" value=""></div>
           <div class="clear" style="width:100%;text-align:center;"><input type="submit" value="Reserve Your Spot Now" name="subscribe" id="mc-embedded-subscribe" class="button orange reserve"></div>
           </div>
       </form>
       </div>
       <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[1]='FNAME';ftypes[1]='text';fnames[0]='EMAIL';ftypes[0]='email';fnames[2]='MMERGE2';ftypes[2]='dropdown';fnames[3]='MMERGE3';ftypes[3]='dropdown';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
-      <!--End mc_embed_signup-->
+      <End mc_embed_signup-->
+
+
+<!-- Begin MailChimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+    #mc_embed_signup{clear:left; font:14px Gotham,Arial,sans-serif; }
+    /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+       We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup" class="form11">
+<form action="//synstudio.us2.list-manage.com/subscribe/post?u=23c298529438530e62af0db46&amp;id=e8c037fe10" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+<div class="mc-field-group">
+    <label for="mce-FNAME">Enter your first name </label>
+    <input type="text" value="" name="FNAME" class="" id="mce-FNAME">
+</div>
+<div class="mc-field-group">
+    <label for="mce-EMAIL">Enter your email address  <span class="asterisk">*</span>
+</label>
+    <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+</div>
+<div class="mc-field-group">
+    <label for="mce-MMERGE2">Select your preferred start date: </label>
+    <select name="MMERGE2" class="" id="mce-MMERGE2">
+    <option value=""></option>
+    <option value="October 2016">October 2016</option>
+<option value="October 2017">October 2017</option>
+
+    </select>
+</div>
+<div class="mc-field-group">
+    <label for="mce-MMERGE3">Select your status: </label>
+    <select name="MMERGE3" class="" id="mce-MMERGE3">
+    <option value=""></option>
+    <option value="Canadian citizen">Canadian citizen</option>
+<option value="Canadian permanent resident">Canadian permanent resident</option>
+<option value="Foreign student">Foreign student</option>
+
+    </select>
+</div>
+<div class="mc-field-group input-group">
+    <ul><li><input type="checkbox" value="1" name="group[9225][1]" id="mce-group[9225]-9225-0"><label for="mce-group[9225]-9225-0">I would like to receive free updates from Syn Studio about the world of Concept Art, including maste</label></li>
+</ul>
+</div>
+    <div id="mce-responses" class="clear">
+        <div class="response" id="mce-error-response" style="display:none"></div>
+        <div class="response" id="mce-success-response" style="display:none"></div>
+    </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_23c298529438530e62af0db46_e8c037fe10" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
+
 
 
             <div class="row">
