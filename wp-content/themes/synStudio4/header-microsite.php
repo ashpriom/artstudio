f36a3e356efbe54ae24822fdd518364b961a6a7c
Ash Priom <sa.priom@gmail.com>
2015-12-04 17:10:09 -0500

    The Facebook Pixel
    added new script in all pages and standard event code for admissions
    page

--- wp-content/themes/synStudio4/header-microsite.php
+++ wp-content/themes/synStudio4/header-microsite.php
@@ -77,23 +77,28 @@
          </script>
 		<!-- End of SkyGlue Code -->
 
+		<!-- Facebook Pixel Code -->
+			<script>
+			!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+			n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+			n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+			t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+			document,'script','//connect.facebook.net/en_US/fbevents.js');
+
+			fbq('init', '456389534556260');
+			fbq('track', "PageView");</script>
+			<noscript><img height="1" width="1" style="display:none"
+			src="https://www.facebook.com/tr?id=456389534556260&ev=PageView&noscript=1"
+			/></noscript>
+		<!-- End Facebook Pixel Code -->
+
+
 		<?php if($postID == 11245 || $postID == 11285){ ?>
-			<!-- Facebook Conversion Code for Application Form Submissions - CADP CANADA 18-30 -->
-			<script>(function() {
-			var _fbq = window._fbq || (window._fbq = []);
-			if (!_fbq.loaded) {
-			var fbds = document.createElement('script');
-			fbds.async = true;
-			fbds.src = '//connect.facebook.net/en_US/fbds.js';
-			var s = document.getElementsByTagName('script')[0];
-			s.parentNode.insertBefore(fbds, s);
-			_fbq.loaded = true;
-			}
-			})();
-			window._fbq = window._fbq || [];
-			window._fbq.push(['track', '6035361214742', {'value':'0.00','currency':'CAD'}]);
+			<script>
+				// Lead
+				// Track when a user expresses interest in your offering (ex. form submission, sign up for trial, landing on pricing page)
+				fbq('track', 'Lead');
 			</script>
-			<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6035361214742&amp;cd[value]=0.00&amp;cd[currency]=CAD&amp;noscript=1" /></noscript>
 		<?php } ?>
 
 		<style>
