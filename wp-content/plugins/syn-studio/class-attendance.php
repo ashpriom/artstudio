3c5725c6f6d13c0e8e6a98e6573e6d67352e746c
Ash Priom <sa.priom@gmail.com>
2015-12-14 17:45:41 -0500

    Syn

--- wp-content/plugins/syn-studio/class-attendance.php
+++ wp-content/plugins/syn-studio/class-attendance.php
@@ -50,7 +50,7 @@ if(isset($_POST['class_instance_id']) && isset($_POST['class_semester_id'])){
 
 			echo "</table>"; ?>
 				<div><p>
-						<input name="student_id_array" type="hidden" value="<?php echo $studentIDArray; ?>"/>
+						<input name="student_id_array" type="hidden" value="<?php $studentIDArray; ?>"/>
 						<input type="submit" name="attendance" class="button-purple" id="synstudio-button" value="Submit Class Attendance"/>
 				</p></div>
 		</form>
