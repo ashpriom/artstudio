f3136f75ec7941e7fbb168a744bd176b3abadd7b
Ash Priom <sa.priom@gmail.com>
2015-12-14 16:10:52 -0500

    Syn - Bootstrap

--- wp-content/plugins/syn-studio/inforoom.php
+++ wp-content/plugins/syn-studio/inforoom.php
@@ -1,16 +1,27 @@
-<div>
-	<h1><b>Syn Studio Information System</b></h1>
-<div>
+<!DOCTYPE html>
+<html lang="en">
 
-<?php
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<script src="js/bootstrap.min.js"></script>
+</head>
 
-	echo "<div id=\"inforoom\" class=\"inforoom-menu\">";
-		echo "<p>Welcome to the Information Room. Here you can manage information about Syn Studio.</p>";
-		echo "<ul>";
-			echo "<li><h2><a href=\"admin.php?page=syn-studio/semesters.php\" target=\"_BLANK\">Semesters</a></h2></li>";
-			echo "<li><h2><a href=\"admin.php?page=syn-studio/classrooms.php\" target=\"_BLANK\">Classrooms</a></h2></li>";
-			echo "<li><h2><a href=\"admin.php?page=syn-studio/students.php\" target=\"_BLANK\">Students</a></h2></li>";
-		echo "</ul>";
-	echo "</div>";
+<body>
+	<div><h1><b>Syn Studio Information System</b></h1><div>
 
-?>
+	<?php
+		echo "<div id=\"inforoom\" class=\"inforoom-menu\">";
+			echo "<p>Welcome to the Information Room. Here you can manage information about Syn Studio.</p>";
+			echo "<ul>";
+				echo "<li><h2><a href=\"admin.php?page=syn-studio/semesters.php\" target=\"_BLANK\">Semesters</a></h2></li>";
+				echo "<li><h2><a href=\"admin.php?page=syn-studio/classrooms.php\" target=\"_BLANK\">Classrooms</a></h2></li>";
+				echo "<li><h2><a href=\"admin.php?page=syn-studio/students.php\" target=\"_BLANK\">Students</a></h2></li>";
+			echo "</ul>";
+		echo "</div>";
+	?>
+
+</body>
+</html>
