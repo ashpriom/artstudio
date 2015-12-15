5db2e0ddc244799fdf3a94e2a9f326cde82350a4
Ash Priom <sa.priom@gmail.com>
2015-12-15 16:39:19 -0500

    Syn

--- wp-content/plugins/syn-studio/class-attendance-db.php
+++ wp-content/plugins/syn-studio/class-attendance-db.php
@@ -6,7 +6,8 @@ if(isset($_POST['student_id_array'])){
 	$studentIDArray = implode(',',$studentIDArray);
 	print_r($studentIDArray);
 	$attendanceArray = $_POST['attendance_array'];
-	//$attendanceArray = implode(',',$attendanceArray);
+	print_r($attendanceArray);
+	$attendanceArray = implode(',',$attendanceArray);
 	print_r($attendanceArray);
 
 	global $wpdb;
@@ -25,12 +26,12 @@ if(isset($_POST['student_id_array'])){
 	);
 
 	$wpdb->print_error();
-	//$location = "admin.php?page=syn-studio%2Fsemesters.php";
-	//wp_safe_redirect($location, $status=302);
 }
 
-else{
-	echo "The semester ID is not with your session anymore. Please go to the <a href=\"/wp-admin/admin.php?page=syn-studio/semesters.php\">Semesters Page</a> and click \"classes\" for a semester.";
+else{ 
+	echo "The semester ID is not with your session anymore. 
+	Please go to the <a href=\"/wp-admin/admin.php?page=syn-studio/semesters.php\">Semesters Page</a> and
+	click \"classes\" for a semester.";
 }
 
 ?>
