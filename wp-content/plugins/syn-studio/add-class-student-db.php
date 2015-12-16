2feb30e3bf5ec9843230ca6dc614f41620b45e89
Ash Priom <sa.priom@gmail.com>
2015-12-16 16:55:40 -0500

    Syn

--- wp-content/plugins/syn-studio/add-class-student-db.php
+++ wp-content/plugins/syn-studio/add-class-student-db.php
@@ -1,36 +1,35 @@
 <?php
 
-if(isset($_POST['semester_id']) && isset($_POST['class_semester_id'])){
-	
-	$semesterID = $_POST['semester_id'];
-	$classSemesterID = $_POST['class_semester_id'];
-	$studentID = $_POST['student_id'];
+	if(isset($_POST['semester_id']) && isset($_POST['class_semester_id'])){
+		
+		$semesterID = $_POST['semester_id'];
+		$classSemesterID = $_POST['class_semester_id'];
+		$studentID = $_POST['student_id'];
 
-	global $wpdb;
-	$$dbstudentID = $wpdb->get_var( "SELECT student_id FROM syn1_syn_enrollment WHERE class_semester_id = $classSemesterID ");
-	
-	if($dbstudentID != $studentID){
-		$classStudentInsert = $wpdb->insert( 
-			'syn1_syn_enrollment',
-			array(
-				'class_semester_id' => sanitize_text_field($classSemesterID),
-				'student_id' => sanitize_text_field($studentID),
-			),
-			array(
-				'%d',
-				'%d',
-			) 
-		);
+		global $wpdb;
+		$dbstudentID = $wpdb->get_var( "SELECT student_id FROM syn1_syn_enrollment WHERE class_semester_id = $classSemesterID ");
+		
+		if($dbstudentID != $studentID){
+			$classStudentInsert = $wpdb->insert( 
+				'syn1_syn_enrollment',
+				array(
+					'class_semester_id' => sanitize_text_field($classSemesterID),
+					'student_id' => sanitize_text_field($studentID),
+				),
+				array(
+					'%d',
+					'%d',
+				)
+			);
 
-		$wpdb->print_error();
-		$location = "admin.php?page=syn-studio%2Fsemesters.php";
-		wp_safe_redirect($location, $status=302);
+			$wpdb->print_error();
+			$location = "admin.php?page=syn-studio%2Fsemesters.php";
+			wp_safe_redirect($location, $status=302);
+		}
+
+		else{ echo "Student already enrolled in this course."; }
 	}
-	else{
-		echo "Student already enrolled in this course.";
-	}
-}
 
-else{ echo "Something is wrong. Please contact system admin. Thank you.";}
+	else{ echo "Something is wrong. Please contact system admin. Thank you.";}
 
 ?>
