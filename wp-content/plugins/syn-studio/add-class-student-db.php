0c53ea55e328c8d3fb103d3f4f212de11088786e
Ash Priom <sa.priom@gmail.com>
2015-12-16 17:28:00 -0500

    Syn

--- wp-content/plugins/syn-studio/add-class-student-db.php
+++ wp-content/plugins/syn-studio/add-class-student-db.php
@@ -7,27 +7,28 @@
 		$studentID = $_POST['student_id'];
 
 		global $wpdb;
-		$dbstudentID = $wpdb->get_var( "SELECT student_id FROM syn1_syn_enrollment WHERE class_semester_id = $classSemesterID ");
+		$dbstudentIDs = $wpdb->get_results( "SELECT student_id FROM syn1_syn_enrollment WHERE class_semester_id = $classSemesterID ");
 		
-		if($dbstudentID != $studentID){
-			$classStudentInsert = $wpdb->insert( 
-				'syn1_syn_enrollment',
-				array(
-					'class_semester_id' => sanitize_text_field($classSemesterID),
-					'student_id' => sanitize_text_field($studentID),
-				),
-				array(
-					'%d',
-					'%d',
-				)
-			);
+		foreach($dbstudentIDs as $dbstudentID){
+			if($dbstudentID->student_id != $studentID){
+				$classStudentInsert = $wpdb->insert( 
+					'syn1_syn_enrollment',
+					array(
+						'class_semester_id' => sanitize_text_field($classSemesterID),
+						'student_id' => sanitize_text_field($studentID),
+					),
+					array(
+						'%d',
+						'%d',
+					)
+				);
 
-			$wpdb->print_error();
-			$location = "admin.php?page=syn-studio%2Fsemesters.php";
-			wp_safe_redirect($location, $status=302);
+				$wpdb->print_error();
+				$location = "admin.php?page=syn-studio%2Fsemesters.php";
+				wp_safe_redirect($location, $status=302);
+			}
+			else{ echo "<p class=\"lead bg-danger\">Student already enrolled in this course. Please go to the <a href=\"/wp-admin/admin.php?page=syn-studio/semesters.php\">Semesters Page</a>.<p>"; }
 		}
-
-		else{ echo "<p class=\"lead bg-danger\">Student already enrolled in this course. Please go to the <a href=\"/wp-admin/admin.php?page=syn-studio/semesters.php\">Semesters Page</a>.<p>"; }
 	}
 
 	else{ echo "Something is wrong. Please contact system admin. Thank you.";}
