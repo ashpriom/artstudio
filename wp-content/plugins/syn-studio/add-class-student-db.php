511947003617b4eb7d5b75577746ee706934b843
Ash Priom <sa.priom@gmail.com>
2015-12-17 16:37:29 -0500

    Syn

--- wp-content/plugins/syn-studio/add-class-student-db.php
+++ wp-content/plugins/syn-studio/add-class-student-db.php
@@ -1,17 +1,15 @@
 <?php
 
 	if(isset($_POST['semester_id']) && isset($_POST['class_semester_id'])){
-		
+
 		$semesterID = $_POST['semester_id'];
-		echo $classSemesterID = $_POST['class_semester_id'];
-		echo $studentID = $_POST['student_id'];
+		$classSemesterID = $_POST['class_semester_id'];
+		$studentID = $_POST['student_id'];
 
 		global $wpdb;
-		//echo $studentIDdb = $wpdb->get_var( "SELECT student_id FROM syn1_syn_enrollment WHERE class_semester_id = $classSemesterID ");
-		//echo $classSemesterIDdb = $wpdb->get_var( "SELECT class_semester_id FROM syn1_syn_enrollment WHERE student_id = $studentID ");
-		//if($studentID != $studentIDdb && $classSemesterID != $classSemesterIDdb){
 		$studentExists = $wpdb->get_results("SELECT enrollment_date FROM syn1_syn_enrollment WHERE student_id = $studentID AND class_semester_id = $classSemesterID ");
-		if (empty($studentExists)){
+		
+		if(empty($studentExists)){
 			$classStudentInsert = $wpdb->insert( 
 				'syn1_syn_enrollment',
 				array(
@@ -26,13 +24,14 @@
 
 			$wpdb->print_error();
 			$location = "admin.php?page=syn-studio%2Fsemesters.php";
-			//wp_safe_redirect($location, $status=302);
+			wp_safe_redirect($location, $status=302);
 		}
 			
-		else {
-			echo "<p class=\"lead bg-danger\">Student already enrolled in this course. Please go to the <a href=\"/wp-admin/admin.php?page=syn-studio/semesters.php\">Semesters Page</a>.<p>"; 
+		else{
+			$getEnrollmentDate = $wpdb->get_var("SELECT enrollment_date FROM syn1_syn_enrollment WHERE student_id = $studentID AND class_semester_id = $classSemesterID ");
+			echo "<p class=\"lead bg-danger\">Student already enrolled in this course on". $getEnrollmentDate.". Please go to the <a href=\"/wp-admin/admin.php?page=syn-studio/semesters.php\"> Students tab from class list for a semester</a>.<p>"; 
 		}
-	
+		
 	}
 
 	else{
