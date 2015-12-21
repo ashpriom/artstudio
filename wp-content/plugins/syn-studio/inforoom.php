d26d1db305f149b81ad14b1f4789af6d73e4bc8c
Ash Priom <sa.priom@gmail.com>
2015-12-21 13:38:29 -0500

    Syn

--- wp-content/plugins/syn-studio/inforoom.php
+++ wp-content/plugins/syn-studio/inforoom.php
@@ -1,20 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<body>
-	<div class="container-fluid">
-		<div class="center-block"><h1><b>Syn Studio Information System</b></h1><div>
-
-		<?php
-			echo "<div id=\"inforoom\" class=\"inforoom-menu\">";
-				echo "<p class=\"lead\">Welcome to the Information Room. Here you can manage information about Syn Studio.</p>";
-				echo "<ul>";
+	<body>
+		<div class="container-fluid">
+			<div class="center-block"><h1><b>Syn Studio Information System</b></h1></div>
+			<div id="inforoom" class="inforoom-menu">
+				<p class=\"lead\">Welcome to the Information Room. Here you can manage information about Syn Studio.</p>
+				<ul>
+					<?php
 					echo "<li><h2><a href=\"admin.php?page=syn-studio/semesters.php\" target=\"_BLANK\">Semesters</a></h2></li>";
 					echo "<li><h2><a href=\"admin.php?page=syn-studio/classrooms.php\" target=\"_BLANK\">Classrooms</a></h2></li>";
 					echo "<li><h2><a href=\"admin.php?page=syn-studio/students.php\" target=\"_BLANK\">Students</a></h2></li>";
-				echo "</ul>";
-			echo "</div>";
-		?>
-	</div>
-</body>
+					?>
+				</ul>
+			</div>
+	
+			<div>
+				<link rel="stylesheet" href="calendar/lib/css/SimpleCalendar.css" />
+				<?php
+					error_reporting(E_ALL ^ E_WARNING);
+					require_once('calendar/lib/donatj/SimpleCalendar.php');
+					$calendar = new donatj\SimpleCalendar();
+					$calendar->setStartOfWeek('Sunday');
+					$calendar->addDailyHtml( 'Sample Event', 'today', 'tomorrow' );
+					$calendar->show(true);
+				?>
+			</div>
+		</div>
+	</body>
 </html>
