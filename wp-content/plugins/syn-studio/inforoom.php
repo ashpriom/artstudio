60dc08c4e81ae55975b67396e263a566abeb0ddc
Ash Priom <sa.priom@gmail.com>
2015-12-21 14:35:03 -0500

    Syn

--- wp-content/plugins/syn-studio/inforoom.php
+++ wp-content/plugins/syn-studio/inforoom.php
@@ -3,10 +3,10 @@
 	<body>
 		<link rel="stylesheet" href="<?php bloginfo('url'); _e(''); ?>/wp-content/plugins/syn-studio/css/bootstrap.min.css" type="text/css" media="screen" />
 		<script src="<?php bloginfo('url'); _e(''); ?>/wp-content/plugins/syn-studio/js/bootstrap.min.js" /></script>
-		<div class="container-fluid">			
+		<div id="inforoom" class="container-fluid">
 			<div class="row">
 				<div class="col-md-5"><h1><b>Syn Studio Information System</b></h1></div>
-				<div id="inforoom" class="inforoom-menu col-md-7">
+				<div class="inforoom-menu col-md-7">
 					<ul>
 						<?php
 						echo "<li><h2><a href=\"admin.php?page=syn-studio/semesters.php\" target=\"_BLANK\">Semesters</a></h2></li>";
