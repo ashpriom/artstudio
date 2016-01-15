3a4c2d5b35a1d8c417fffd3f1b7822e430fb3bc0
Ash Priom <sa.priom@gmail.com>
2016-01-15 14:21:51 -0500

    Workshop

--- wp-content/themes/synStudio4/header-masterClass4.php
+++ wp-content/themes/synStudio4/header-masterClass4.php
@@ -2,68 +2,59 @@
 /**
  * @package WordPress
  * @subpackage SynStudio_Theme
- */
-	
+ */	
 ?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
-<head>
-<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-<meta name="author" content="Anthony Walsh, Courtney Clinton, Syed Ashfaque Uddin Priom" />
-<meta name="copyright" content="Syn Studio" />
-<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
-<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
-<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+	<head>
+		<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+		<meta name="author" content="Anthony Walsh, Courtney Clinton, Syed Ashfaque Uddin Priom" />
+		<meta name="copyright" content="Syn Studio" />
+		<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+		<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/images/favicon.ico" />
+		
+		<?php wp_enqueue_script("jquery"); ?>
+		<?php wp_head(); ?>
 
-<?php wp_enqueue_script("jquery"); ?>
-<?php wp_head(); ?>
+		<?php 
+		if (function_exists('pll_current_language')) { $currentLang = pll_current_language('slug'); }
+		if ($currentLang == "fr") { ?><link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style-fr.css" type="text/css" media="screen" /><?php } 
+		?>
 
-<?php 
-if (function_exists('pll_current_language')) { $currentLang = pll_current_language('slug'); }
+		<style>
+			#mobile-header { display: none; }
+			@media only screen and (max-width: 768px){
+				#mobile-header { display: block;}
+				#nav-wrapper { display: none; }
+			}
+		</style>
 
-if ($currentLang == "fr") { ?>
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style-fr.css" type="text/css" media="screen" />
-<?php } ?>
+		<script type="text/javascript">
+			jQuery(document).ready(function (){
+				jQuery("li#menu-item-5944 a").append("<div class='classNotOffered' style='padding: 16px 8px'>Class Not Offered for this semester</div>");
+				jQuery("li#menu-item-5944 a .classNotOffered").hide();
+				jQuery("li#menu-item-5944").mouseover(function() {
+					jQuery("li#menu-item-5944 a .classNotOffered").show();
+				}).mouseout(function(){
+					jQuery("li#menu-item-5944 a .classNotOffered").hide();
+				});
+				
+				//FRENCH Advanced Production Techniques
+				jQuery("li#menu-item-4767 a").append("<div class='classNotOffered' style='padding: 16px 8px'>Ce cours n&rsquo;est pas offert cette session</div>");
+				jQuery("li#menu-item-4767 a .classNotOffered").hide();
+				jQuery("li#menu-item-4767").mouseover(function() {
+					jQuery("li#menu-item-4767 a .classNotOffered").show();
+				}).mouseout(function(){
+					jQuery("li#menu-item-4767 a .classNotOffered").hide();
+				});
+			});
+		</script>
+	</head>
 
-<style>
-	#mobile-header { display: none; }
-	@media only screen and (max-width: 768px){
-		#mobile-header { display: block;}
-		#nav-wrapper { display: none; }
-	}
-</style>
-
-<script type="text/javascript">
-jQuery(document).ready(function () {
-// For mobile testing $("#menu-main-nav").tinyNav();
-
-	//Advanced Production Techniques
-	jQuery("li#menu-item-5944 a").append("<div class='classNotOffered' style='padding: 16px 8px'>Class Not Offered for this semester</div>");
-	jQuery("li#menu-item-5944 a .classNotOffered").hide();
-	jQuery("li#menu-item-5944").mouseover(function() {
-		jQuery("li#menu-item-5944 a .classNotOffered").show();
-	}).mouseout(function(){
-		jQuery("li#menu-item-5944 a .classNotOffered").hide();
-	});
-	
-	//FRENCH Advanced Production Techniques
-	jQuery("li#menu-item-4767 a").append("<div class='classNotOffered' style='padding: 16px 8px'>Ce cours n&rsquo;est pas offert cette session</div>");
-	jQuery("li#menu-item-4767 a .classNotOffered").hide();
-	jQuery("li#menu-item-4767").mouseover(function() {
-		jQuery("li#menu-item-4767 a .classNotOffered").show();
-	}).mouseout(function(){
-		jQuery("li#menu-item-4767 a .classNotOffered").hide();
-	});
-	
-});
-
-</script>
-
-<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/images/favicon.ico" />
-
-</head>
-
-<body>
+	<body>
 		<!-- Google Tag Manager -->
         <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-MJ8KT7"
         height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
@@ -74,57 +65,52 @@ jQuery(document).ready(function () {
         })(window,document,'script','dataLayer','GTM-MJ8KT7');</script>
         <!-- End Google Tag Manager -->
 
-<?php remove_filter ('the_content', 'wpautop'); ?>
+		<?php remove_filter ('the_content', 'wpautop'); ?>
 
-<!-- Main Content Wrapper: Start -->
-<div id="wrapper">
- <!-- header: start -->
-	<div id="header">
- 	<!-- banner: start -->
+		<div id="wrapper">
+			<div id="header">
+			
 				<?php
 				if ($currentLang == "fr") { ?>
-					<img src="<?php bloginfo('url'); ?>/wp-content/uploads/2016/01/gathering-of-masters.jpg\" alt="SYN STUDIO - GATHERING OF MASTERS 2016" width="1000" />
+					<img src="<?php bloginfo('url'); ?>/wp-content/uploads/2016/01/gathering-of-masters.jpg" alt="SYN STUDIO - GATHERING OF MASTERS 2016" width="1000" />
 				<?php }
 				else { ?>
-					<img src="<?php bloginfo('url'); ?>/wp-content/uploads/2016/01/gathering-of-masters.jpg\" alt="SYN STUDIO - GATHERING OF MASTERS 2016" width="1000" />
+					<img src="<?php bloginfo('url'); ?>/wp-content/uploads/2016/01/gathering-of-masters.jpg" alt="SYN STUDIO - GATHERING OF MASTERS 2016" width="1000" />
 				<?php } ?>  
-  	<!-- banner: end -->
-  	<div id="language2" style="top: 270px; right: 12px; color:white;">
-   	<?php if (function_exists('pll_the_languages')){ 
-   		pll_the_languages(array('hide_current'=>1, 'show_flags'=>1)); }
-   	?>
-  	</div>
-  
-  <div id="nav">
-  	<div id="nav-wrapper">
-				<?php
-				if ($currentLang == "fr") {
-					wp_nav_menu( array('menu' => 'Main Nav FR' ));
-				}
-				else {
-					wp_nav_menu( array('menu' => 'Main Nav' ));
-				}
-				
-				?>
-   </div>
-   
-   <div id="social">
-			<?php
-			 echo "<h4>&nbsp;</h4>";
-				//if ($currentLang == "fr") {
-				//	echo "<h4>GARDEZ LE CONTACT</h4>";
-				//}
-				//else {
-				//	echo "<h4>Stay in touch</h4>";
-				//}
-				
-				?>   
-   	<a href="http://www.facebook.com/SynStudio" target="_blank" id="lnk-facebook">Facebook</a>
-    <a href="https://twitter.com/SynStudio" target="_blank" id="lnk-twitter">Twitter</a>
-    <a href="https://www.youtube.com/user/SynStudioMontreal" target="_blank" id="lnk-youtube">You Tube</a>
-
-   </div>
-  </div>
- 
- </div>
- <!-- header: end -->
+		  	
+			  	<div id="language2" style="top: 270px; right: 12px; color:white;">
+			   		<?php 
+			   		if (function_exists('pll_the_languages')){ 
+			   			pll_the_languages(array('hide_current'=>1, 'show_flags'=>1)); 
+			   		}
+			   		?>
+			  	</div>
+		  
+		  		<div id="nav">
+		  			<div id="nav-wrapper">
+						<?php
+						if ($currentLang == "fr") {
+							wp_nav_menu( array('menu' => 'Main Nav FR' ));
+						}
+						else {
+							wp_nav_menu( array('menu' => 'Main Nav' ));
+						}
+						?>
+		   			</div>
+		   
+		   		<div id="social">
+					<?php
+					 echo "<h4>&nbsp;</h4>";
+						//if ($currentLang == "fr") {
+						//	echo "<h4>GARDEZ LE CONTACT</h4>";
+						//}
+						//else {
+						//	echo "<h4>Stay in touch</h4>";
+						//}
+					?>   
+				   	<a href="http://www.facebook.com/SynStudio" target="_blank" id="lnk-facebook">Facebook</a>
+				    <a href="https://twitter.com/SynStudio" target="_blank" id="lnk-twitter">Twitter</a>
+				    <a href="https://www.youtube.com/user/SynStudioMontreal" target="_blank" id="lnk-youtube">You Tube</a>
+		   		</div>
+		  	</div>
+		</div>
