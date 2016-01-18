bbd8f3e1de8d9200d20eb79e5ed926a6c8064879
Ash Priom <sa.priom@gmail.com>
2016-01-18 18:17:55 -0500

    Transition

--- wp-content/themes/synStudio4/cadiploma-admission.php
+++ wp-content/themes/synStudio4/cadiploma-admission.php
@@ -32,6 +32,7 @@ Template Name: Concept Art Diploma - Admission
 </script>
 
 <script type="text/javascript">
+
     /*jQuery(document).ready(function(){
       $('.scroll-link').on('click', function(event){
         event.preventDefault();
@@ -74,6 +75,7 @@ Template Name: Concept Art Diploma - Admission
         }
         });
     });*/
+
 </script>
 
 <body>
@@ -932,7 +934,7 @@ Template Name: Concept Art Diploma - Admission
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    
+
                                     <!--<tr class="grey-table-row">
                                         <td>1</td>
                                         <td>Submit for the Initial Portfolio/Application Review (receive feedback from a professional concept artist and the chance to reapply free of charge if your portfolio doesn’t pass the review)</td>
