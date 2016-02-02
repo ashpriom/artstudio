683c004ca777936e4425291e95674f66723df44a
Syed <sa.priom@gmail.com>
2016-02-02 13:42:19 -0500

    Add art in source

--- wp-content/themes/synStudio4/header.php
+++ wp-content/themes/synStudio4/header.php
@@ -6,6 +6,18 @@
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<!--
+     _______.____    ____ .__   __.         _______..___________. __    __   _______   __    ______   
+    /       |\   \  /   / |  \ |  |        /       ||           ||  |  |  | |       \ |  |  /  __  \  
+   |   (----` \   \/   /  |   \|  |       |   (----``---|  |----`|  |  |  | |  .--.  ||  | |  |  |  | 
+    \   \      \_    _/   |  . `  |        \   \        |  |     |  |  |  | |  |  |  ||  | |  |  |  | 
+.----)   |       |  |     |  |\   |    .----)   |       |  |     |  `--'  | |  '--'  ||  | |  `--'  | 
+|_______/        |__|     |__| \__|    |_______/        |__|      \______/  |_______/ |__|  \______/  
+                                                                                                      
+
+-->
+
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
 	<head>
 		<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
