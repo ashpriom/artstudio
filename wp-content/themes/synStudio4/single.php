bd432c2d364e7b99a01d17c58d233d0d427747c0
Syed <sa.priom@gmail.com>
2016-04-18 12:28:32 -0400

    Pragmatics

--- wp-content/themes/synStudio4/single.php
+++ wp-content/themes/synStudio4/single.php
@@ -3,7 +3,7 @@
 	Load different templates based on the type of class we offer. Every page load request goes through this file to load php templates.
 */
 
-$postID = get_the_ID(); 
+$postID = get_the_ID();
 
 if($postID==6502){ get_header("masterClass");} // activate header-masterClass.php, header-masterClass2.php and so on.
 elseif($postID==10088){ get_header("masterClass2"); }
@@ -13,9 +13,9 @@ elseif ($postID==12457 || $postID==12459) { get_header("masterClassArtstation");
 elseif ($postID==12458 || $postID==12460) { get_header("masterClassWacom"); }
 else{ get_header(); }
 
-if ( have_posts() ) { the_post(); rewind_posts(); }
+if(have_posts()){ the_post(); rewind_posts();}
 
-if ( in_category(4) || in_category(48) ) { // This is for all pages in "class (cours)" category
+if(in_category(4) || in_category(48)){ // This is for all pages in "class (cours)" category
 	if ($postID==6502) { include(TEMPLATEPATH . '/singleClassMaster.php'); }
 	elseif ($postID==12105 || $postID==12106 || $postID==12457 || $postID==12458 || $postID==12459 || $postID==12460) { include(TEMPLATEPATH . '/masterclass.php'); }
 	elseif ($postID==8397 || $postID==11019) { include(TEMPLATEPATH . '/singleIntenseClassDetail.php'); } // this is template for full time intensive
