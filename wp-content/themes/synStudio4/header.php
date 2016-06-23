3ed1159daf17e153dafa68134869f006996c83d0
Ash Priom <sa.priom@gmail.com>
2016-06-23 15:16:21 -0400

    Like Box
    Moved script to header

--- wp-content/themes/synStudio4/header.php
+++ wp-content/themes/synStudio4/header.php
@@ -102,6 +102,16 @@
 	        })(window,document,'script','dataLayer','GTM-MJ8KT7');</script>
     	<!-- End Google Tag Manager -->
 
+    	<div id="fb-root"></div>
+      
+      <script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+
 		<div id="wrapper"> <!-- Main Content Wrapper: Start, ends in footer -->
 		<div id="header"> <!-- header: start -->
 
