97fb89ef4ab963206ba7979e8c39c0ff08939369
Ash Priom <sa.priom@gmail.com>
2016-06-23 15:09:20 -0400

    Facebook Like Box and Progressive Image

--- wp-content/themes/synStudio4/sidebar.php
+++ wp-content/themes/synStudio4/sidebar.php
@@ -7,57 +7,77 @@
  */
 
 if (function_exists('pll_current_language')) { $currentLang = pll_current_language('slug'); }
+
 ?>
 
-<!-- column 2: start -->
-<div id="col2">
+
+<div id="col2"> <!-- column 2: start -->
 
   <?php
 
-if($currentLang=="en"){
-  $catID = '10';
-  query_posts(array(
-		'showposts' => 1,
-		'orderby' => 'date',
-    'order' => 'desc',
-		'category_name' => 'Testimonials' // Code Reuse: You can insert any category name
-	));
-}
+    if($currentLang=="en"){
+      $catID = '10';
+      query_posts(array(
+    		'showposts' => 1,
+    		'orderby' => 'date',
+        'order' => 'desc',
+    		'category_name' => 'Testimonials' // Standard WordPress Category Query. Replace category name to  reuse in other pages
+    	));
+    }
 
-else{
-  $catID = '110';
-  query_posts(array(
-    'showposts' => 1,
-    'orderby' => 'date',
-    'order' => 'desc',
-    'category_name' => 'testimonials-fr' // Code Reuse: You can insert any category name
-  ));
-}
+    else{
+      $catID = '110';
+      query_posts(array(
+        'showposts' => 1,
+        'orderby' => 'date',
+        'order' => 'desc',
+        'category_name' => 'testimonials-fr'
+      ));
+    }
 
-  while (have_posts()) : the_post();
-    echo "<div class='info-box2'>";
-    echo '<h3>' . get_cat_name($catID) . '</h3>';
-    echo "<div class='text-wrapper'><div class='text'>";
-    the_excerpt();
-    if ($currentLang == "en") { echo "<a class='lnk-readMore' href='/testimonials'>Read More</a>"; }
-		else{ echo "<a class='lnk-readMore' href='/temoignages/'>En savoir plus</a>"; }
-    echo "</div></div>";
-    echo "</div>";
-  endwhile;
+    while (have_posts()) : the_post();
+      echo "<div class='info-box2'>";
+      echo '<h3>' . get_cat_name($catID) . '</h3>';
+      echo "<div class='text-wrapper'><div class='text'>";
+      the_excerpt();
+      if ($currentLang == "en") { echo "<a class='lnk-readMore' href='/testimonials'>Read More</a>"; }
+  		else{ echo "<a class='lnk-readMore' href='/temoignages/'>En savoir plus</a>"; }
+      echo "</div></div>";
+      echo "</div>";
+    endwhile;
 
   ?>
 
   <div id="share-plugins">
-
+    
     <div id="facebook">
-		  <iframe src="http://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.facebook.com/SynStudio&amp;send=false&amp;layout=box_count&amp;width=55&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=90&amp;appId=223615814335068" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:65px; height:90px;" allowTransparency="true"></iframe>
+		  
+      <div id="fb-root"></div>
+      
+      <script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+
+      <div class="fb-like" 
+        data-href="https://www.facebook.com/SynStudio" data-width="55" data-layout="box_count" 
+        data-action="like" data-show-faces="false" data-share="false">
+      </div>
+
+      <!--<iframe src="http://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.facebook.com/SynStudio&amp;send=false&amp;
+      layout=box_count&amp;width=55&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=90&amp;appId=223615814335068" 
+      scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:65px; height:90px;" allowTransparency="true"></iframe>-->
+
     </div>
 
     <div id="twitter">
       <div class="twitter-sideBar">
         <?php
-        echo getTwitterFollowers('SynStudio'); // see functions.php for reference
-        echo '<div class="followers">Followers</div>';
+          echo getTwitterFollowers('SynStudio'); // see functions.php for reference
+          echo '<div class="followers">Followers</div>';
         ?>
         <a href="https://twitter.com/share" class="twitter-share-button" data-dnt="true" data-count="none" data-via="SynStudio" data-text="I'm checking out Syn Studio Art School right now!">Tweet</a>
         <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
