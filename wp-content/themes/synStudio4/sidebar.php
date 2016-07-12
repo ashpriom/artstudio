ac398a9573707175ce9feb02c98ce256d9e59d65
Ash Priom <sa.priom@gmail.com>
2016-07-12 15:22:02 -0400

    Share Buttons Repositioning

--- wp-content/themes/synStudio4/sidebar.php
+++ wp-content/themes/synStudio4/sidebar.php
@@ -51,7 +51,7 @@ if (function_exists('pll_current_language')) { $currentLang = pll_current_langua
   <div id="share-plugins">
     
     <!--<div id="facebook">-->
-    <div style="margin-bottom:10px; margin-top:40px;">
+    <div style="margin-bottom:10px;">
 
       <div id="fb-root"></div>
       
