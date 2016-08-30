de2b00d5e11ce5dcb4a8fa2143c01065054cb756
Ash Priom <sa.priom@gmail.com>
2016-08-30 16:07:12 -0400

    Refactoring
    Removed themes and unused font files

--- wp-content/themes/synStudio4/page.php
+++ wp-content/themes/synStudio4/page.php
@@ -3,27 +3,23 @@
  * @package WordPress
  * @subpackage SynStudio_Theme
  */
+?>
 
-get_header(); ?>
+<?php get_header(); ?>
 
-	<!-- content: start -->
-	<div id="content" class="general-page">
- 	<!-- column 1: start -->
- 	<div id="col1">  
-				<?php while ( have_posts() ) : the_post(); ?>
-				<?php echo '<h1>' . get_the_title() . '</h1>'; ?>
- 	 	<div class='podcast-boxWrapper'>
-   		<div class='podcast-box'>
-					<?php the_content(); ?>
-     </div>
-     </div>
-
-				<?php endwhile; // end of the loop. ?>
-  </div>
-  <!-- column 1: end --> 
+<div id="content" class="general-page"> <!-- content: start --> 
+ 	
+ 	<div id="col1"> <!-- column 1: start -->
+		<?php while ( have_posts() ) : the_post(); ?>
+			<?php echo '<h1>' . get_the_title() . '</h1>'; ?>
+ 	 		<div class='podcast-boxWrapper'>
+   				<div class='podcast-box'><?php the_content(); ?></div>
+     		</div>
+		<?php endwhile; ?>
+  	</div> <!-- column 1: end --> 
   
-  <?php get_sidebar(); ?>	
-	</div>
-	<!-- content: end -->
+  	<?php get_sidebar(); ?>
+
+</div> <!-- content: end -->
 
 <?php get_footer(); ?>
