9e169242dce7a16d94f22d3338d919eed437108a
Ash Priom <sa.priom@gmail.com>
2016-09-01 16:41:35 -0400

    AMP Resolve

--- wp-content/themes/synStudio4/header.php
+++ wp-content/themes/synStudio4/header.php
@@ -18,12 +18,24 @@
 
 -->
 
+<<<<<<< HEAD
 <html ⚡ <?php language_attributes(); ?>>
 	<head>
 		<meta charset="utf-8">
 		<!--<meta http-equiv="Content-Type" content="<?php // bloginfo('html_type'); ?>" />-->
+=======
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+	<head>
+		<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+		<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+>>>>>>> parent of 3a9b444... Start AMP
 		<meta name="viewport" content="width=device-width,user-scalable=no,minimum-scale=1,maximum-scale=1,initial-scale=1"/>
 		<meta itemprop="name" content="<?php the_title(); ?>"/>
+		<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/images/favicon.ico" />
+		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/jquery.sidr.light.css" type="text/css">
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/synstudioResponsive.min.css" type="text/css" media="screen">
+		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 		<meta name="author" content="Andrea Acosta Duarte, Bill Jamshedji, Syed Priom" />
 		<meta name="copyright" content="<?php bloginfo('url'); _e(''); ?>" />
 		<meta name="p:domain_verify" content="ed36e341a2434aae18c7121607bc9247"/>
@@ -37,6 +49,7 @@
 		<meta property="og:url" content="<?php the_permalink(); ?>" />
 		<meta property="og:type" content="article" />
 		<meta property="og:title" content="<?php the_title(); ?>" />
+<<<<<<< HEAD
 		<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
 		<link rel="canonical" href="<?php the_permalink(); ?>" />
 		<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/images/favicon.ico" />
@@ -44,15 +57,28 @@
 		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/jquery.sidr.light.css" type="text/css">
 		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/synstudioResponsive.min.css" type="text/css" media="screen">
+=======
+		
+		<script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+		
+		<?php wp_enqueue_script("jquery"); ?>
+		
+		<?php
+			$options = get_option( 'sample_theme_options' ); // loads theme options used for registration dedline notices. See theme_options_do_page() in functions.php
+		?>
+		
+		<?php wp_head(); ?>
+>>>>>>> parent of 3a9b444... Start AMP
 
-		<!-- Detect page language with Polylang polylang.wordpress.com/documentation/documentation-for-developers/functions-reference/ -->
-		<?php if (function_exists('pll_current_language')) { $currentLang = pll_current_language(slug); } ?>
-		<?php if ($currentLang == "fr") { ?>
-			<Style>
-				.info-box1 h2 {font-size: 1.6em;}
-				.info-box10 .mailform2{width: 350px !important; }
-			</style>
-		<?php } ?>
+		<?php
+			if (function_exists('pll_current_language')) {
+				$currentLang = pll_current_language(slug); // See polylang documentation polylang.wordpress.com/documentation/documentation-for-developers/functions-reference/
+			}
+			if ($currentLang == "fr") { ?>
+				<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style-fr.css" type="text/css" media="screen" /> <?php
+			} ?>
+
+		<script src="<?php echo get_template_directory_uri(); ?>/js/jquery_cookie.min.js"></script>
 
 		<style>
 			#mobile-header { display: none; }
@@ -62,17 +88,7 @@
 			}
 		</style>
 
-		<!-- AMP Boilerplate Style -->
-		<style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style>
-		<noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
-
-		<script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
-		<?php wp_enqueue_script("jquery"); ?>		
-		<?php $options = get_option( 'sample_theme_options' ); // Custom theme options. See theme_options_do_page() in functions.php ?>
-		<?php wp_head(); ?>
-
 		<!-- Popup Message Cookie -->
-		<script src="<?php echo get_template_directory_uri(); ?>/js/jquery_cookie.min.js"></script>
 		<script type="text/javascript">
 			jQuery( document ).ready(function( $ ) {
 			 	$("#close-msg").bind( "click", function() {
@@ -92,9 +108,12 @@
 			 var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(sg, s);
 		   }, 1);
 		</script>
+<<<<<<< HEAD
 
 		<!-- Load AMP JS -->
 		<script async src="https://cdn.ampproject.org/v0.js"></script>
+=======
+>>>>>>> parent of 3a9b444... Start AMP
 	
 	</head>
 
@@ -144,12 +163,11 @@
 			</div>
 
 			<div id="language">
-	   			<div class="phone">514 998-7625</div>
-	   			<?php 
-	   			if (function_exists('pll_the_languages')){
-	   				pll_the_languages(array('hide_current'=>1, 'show_flags'=>1)); // See Polylang reference
+   			<div class="phone">514 998-7625</div>
+   			<?php if (function_exists('pll_the_languages')){
+   				pll_the_languages(array('hide_current'=>1, 'show_flags'=>1)); // See Polylang reference
 				}
-	   			?>
+   			?>
 			</div>
 
 			<div id="nav">
@@ -160,19 +178,24 @@
 
 		  		<div id="nav-wrapper">
 					<?php
-					if ( $currentLang == "fr"){ wp_nav_menu( array('menu' => 'Main Nav FR' ));}
-					else{ wp_nav_menu( array('menu' => 'Main Nav' )); }
+					if ($currentLang == "fr"){
+						wp_nav_menu( array('menu' => 'Main Nav FR' ));
+					}
+					else{
+						wp_nav_menu( array('menu' => 'Main Nav' ));
+					}
 					?>
 		   		</div>
 
 		   		<div id="social">
-					<?php 
-					if ($currentLang == "fr") { echo "<h4>GARDEZ LE CONTACT</h4>"; }
-					else { echo "<h4>Stay in touch</h4>"; }
+					<?php
+						if ($currentLang == "fr") { echo "<h4>GARDEZ LE CONTACT</h4>"; }
+						else { echo "<h4>Stay in touch</h4>"; }
 					?>
-				  	<a href="http://www.facebook.com/SynStudio" target="_blank" title="Facebook" id="lnk-facebook">Facebook</a>
+					<!-- Social Media -->
+				  <a href="http://www.facebook.com/SynStudio" target="_blank" title="Facebook" id="lnk-facebook">Facebook</a>
 					<a href="https://twitter.com/SynStudio" target="_blank" title="Twitter" id="lnk-twitter">Twitter</a>
-				  	<a href="https://www.youtube.com/user/SynStudioMontreal" target="_blank" title="You Tube" id="lnk-youtube">YouTube</a>
+				  <a href="https://www.youtube.com/user/SynStudioMontreal" target="_blank" title="You Tube" id="lnk-youtube">YouTube</a>
 				</div>
 			
 			</div>
