b797883e53f6174b41139ac1fa4c28cefa3d7f3d
Ash Priom <sa.priom@gmail.com>
2016-09-06 16:57:00 -0400

    Concate Alt Image URL

--- wp-content/themes/synStudio4/header-microsite.php
+++ wp-content/themes/synStudio4/header-microsite.php
@@ -9,7 +9,7 @@
 $postID = get_the_ID();
 $metaTitle = get_post_meta($postID, 'meta_title', true);
 $metaDesc = get_post_meta($postID, 'meta_description', true);
-$metaImage = \"bloginfo('template_directory');/images/bannerhome.jpg";
+$metaImage .= bloginfo('template_directory'); . "/images/bannerhome.jpg";
 ?>
 
 <!DOCTYPE html>
