512a8d6e510917d1ef6ef5c1865e8d5679ba99ee
Ash Priom <sa.priom@gmail.com>
2016-09-07 16:57:27 -0400

    Attachment URL

--- wp-content/themes/synStudio4/header.php
+++ wp-content/themes/synStudio4/header.php
@@ -18,7 +18,7 @@ if(function_exists('pll_get_post')){ $translationID = pll_get_post($postID,'en')
 if(in_category(4) || in_category(48)){
 	if($currentLang=="en"){ $attachment_id = get_post_meta($postID, 'thumbnail', true); }
 	else{ $attachment_id = get_post_meta($translationID, 'thumbnail', true); }
-	$metaImageAtttributes = wp_get_attachment_image_src($attachment_id); // Returns an array containing image info
+	$metaImageAtttributes = wp_get_attachment_image_url($attachment_id,'large',false); // Returns an array containing image info
 	$metaImage = $metaImageAtttributes[0];
 }
 else{ $templateDir = get_template_directory_uri(); $metaImage .= $templateDir ."/css/images/synlogo.jpg"; }
