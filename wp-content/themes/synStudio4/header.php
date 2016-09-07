cae46cd8a4b6cad29c63184a4f785db1d89699b7
Ash Priom <sa.priom@gmail.com>
2016-09-07 16:41:04 -0400

    Get iamge source using CFS

--- wp-content/themes/synStudio4/header.php
+++ wp-content/themes/synStudio4/header.php
@@ -18,7 +18,7 @@ if(function_exists('pll_get_post')){ $translationID = pll_get_post($postID,'en')
 if(in_category(4) || in_category(48)){
 	if($currentLang=="en"){ $attachment_id = get_post_meta($postID, 'thumbnail', true); }
 	else{ $attachment_id = get_post_meta($translationID, 'thumbnail', true); }
-	$metaImage = wp_get_attachment_image($attachment_id, array('545', '342'), "", "" ));
+	$metaImage = wp_get_attachment_image($attachment_id, array('545', '342'), "", "");
 }
 else{ $templateDir = get_template_directory_uri(); $metaImage .= $templateDir ."/css/images/synlogo.jpg"; }
 
