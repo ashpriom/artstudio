1f2958988c457f963cd3f6da38069573c7b58471
Syed Priom <sa.priom@gmail.com>
2016-09-14 14:59:56 -0400

    Facebook Pixel

--- wp-content/themes/synStudio4/header-microsite.php
+++ wp-content/themes/synStudio4/header-microsite.php
@@ -103,7 +103,7 @@ $templateDir = get_template_directory_uri(); $metaImage .= $templateDir ."/image
         	</script>
 		<!-- End of SkyGlue Code -->
 
-		<!-- Facebook Pixel Code -->
+		<!-- Facebook Pixel Code - dec 2015 -->
 			<script>
 			!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 			n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
@@ -115,6 +115,21 @@ $templateDir = get_template_directory_uri(); $metaImage .= $templateDir ."/image
 			fbq('track', "PageView");</script>
 			<noscript><img height="1" width="1" style="display:none"
 			src="https://www.facebook.com/tr?id=456389534556260&ev=PageView&noscript=1"
+			/></noscript>
+		<!-- End Facebook Pixel Code -->
+
+		<!-- Facebook Pixel Code - sep 2016 -->
+			<script>
+			!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+			n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+			n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+			t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+			document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+			fbq('init', '1601202580180253');
+			fbq('track', "PageView");</script>
+			<noscript><img height="1" width="1" style="display:none"
+			src="https://www.facebook.com/tr?id=1601202580180253&ev=PageView&noscript=1"
 			/></noscript>
 		<!-- End Facebook Pixel Code -->
 
