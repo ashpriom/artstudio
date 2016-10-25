e66771fdc3cd526edc2e91b9f3bcfed0939294bf
Syed Priom <sa.priom@gmail.com>
2016-10-25 17:49:39 -0400

    FR slide link

--- wp-content/themes/synStudio4/singleClassDetail.php
+++ wp-content/themes/synStudio4/singleClassDetail.php
@@ -247,7 +247,7 @@
 					    	echo "slideLink:\"".$field['link'];
 					    }
 					    else{
-					    	echo "slideLink:\"".$field['link']."/fr";
+					    	echo "slideLink:\"".get_permalink();
 					    }
 					    echo "\",\n";
 					    echo "}; \n";
