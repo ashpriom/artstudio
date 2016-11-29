b7bf9b582379c8463aa107e092b754fa98bf4e16
Syed Priom <sa.priom@gmail.com>
2016-11-29 15:46:13 -0500

    Masterclass Template JQuery conflict

--- wp-content/themes/synStudio4/masterclass.php
+++ wp-content/themes/synStudio4/masterclass.php
@@ -6,7 +6,7 @@
 ?>
 
 <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/slideshow.min.css" />
-<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery-1.4.2.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
 <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.jcarousel.pack.js"></script>
 <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery_cookie.min.js"></script>
 
@@ -178,18 +178,22 @@
 						$fields = CFS()->get('slider',$translationID);
 					}
 					$i=0;
-					foreach ((array)$fields as $field){
+					foreach ((array)$fields as $field) {
 						$i++;
 						echo "var slide".$i." = { \n";
 					    echo "slideLarge:\"".$field['image'];
 					    echo "\",\n";
+
+					    $slideID = get_attachment_id($field['image']);
+					    $thumbSource = wp_get_attachment_image_src($slideID,'slidethumb',false);
+
 					    if($field['small_image']==""){
-					    	echo "slideSmall:\"".$field['image'];
+					    	echo "slideSmall:\"".$thumbSource[0];
 						}
 						else{
 							echo "slideSmall:\"".$field['small_image'];
 						}
-					    echo "\",\n";	    
+					    echo "\",\n";    
 					    if($currentLang=="en"){
 					    	echo "slideText1:\"".$field['text1'];
 					    }
