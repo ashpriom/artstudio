473fe32ecc61d7263e68926764771731e166fe2e
Syed Priom <sa.priom@gmail.com>
2017-01-17 16:32:18 -0500

    Slideshow bug

--- wp-content/themes/synStudio4/singleClassDetail.php
+++ wp-content/themes/synStudio4/singleClassDetail.php
@@ -292,11 +292,17 @@
 					    echo "slideLarge:\"".$field['image'];
 					    echo "\",\n";
 
-					    $slideID = get_attachment_id($field['image']);
+					    //$slideID = get_attachment_id($field['image']);
+					    $slideID = attachment_url_to_postid($field['image']);
 					    $thumbSource = wp_get_attachment_image_src($slideID,'slidethumb',false);
 					    
 					    if(empty($field['small_image'])){
-					    	echo "slideSmall:\"".$thumbSource[0];
+					    	if(!empty($thumbSource[0]){
+					    		echo "slideSmall:\"".$thumbSource[0];
+					    	}
+					    	else{
+					    		echo "slideSmall:\"".$field['image'];
+					    	}
 						}
 						else{
 							echo "slideSmall:\"".$field['small_image'];
