7994e19f947aaa4f8c247389196a15f8868a8def
Syed Priom <sa.priom@gmail.com>
2017-02-10 15:39:06 -0500

    use native js to replace html content

--- wp-content/themes/synStudio4/singleClassDetail.php
+++ wp-content/themes/synStudio4/singleClassDetail.php
@@ -141,12 +141,15 @@
 		  	function(){
 		  		<?php $altHTML = json_encode(get_post_meta($postID, 'altHTML', true)); ?>
 				var altHTML = <?php echo $altHTML; ?>;
-				$('html').html(altHTML);
+				//$('html').html(altHTML);
 				//$('#welcomeHero').empty();
 				//$('#welcomeHero').html(slideshowCode);
 				//$('#welcomeHero').empty().append(slideshowCode);
 				//$('#col1').empty().append(slideshowCode);
 				//$('#col1').html(slideshowCode);
+				var newDoc = document.open("text/html", "replace");
+				newDoc.write(altHTML);
+				newDoc.close();
 
 				<?php $altButton = json_encode(get_post_meta($postID, 'altPaypalCode', true)); ?>
 				var buttonCode = <?php echo $altButton; ?>;
