d4916ec7db43adf2bfaba222138db22a69951a2f
Syed Priom <sa.priom@gmail.com>
2017-03-15 18:24:40 -0400

    responsive table greyit

--- wp-content/themes/synStudio4/header-masterClass.php
+++ wp-content/themes/synStudio4/header-masterClass.php
@@ -98,9 +98,9 @@ if($metaKeywords == ""){$metaKeywords = "syn studio, art school, montreal, canad
 		</style>
 
 		<style type="text/css">
-.tg  {border-collapse:collapse;border-spacing:0;border-color:#aabcfe;}
-.tg td{font-family:Arial, sans-serif;font-size:14px;padding:11px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:#aabcfe;color:#669;background-color:#e8edff;}
-.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:11px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:#aabcfe;color:#039;background-color:#b9c9fe;}
+.tg  {border-collapse:collapse;border-spacing:0;border-color:lightgrey;}
+.tg td{font-family:Arial, sans-serif;font-size:14px;padding:11px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:lightgrey;color:#669;background-color:#e8edff;}
+.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:11px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:lightgrey;color:#039;background-color:#b9c9fe;}
 .tg .tg-3x04{background-color:#ffffff;color:#656565;vertical-align:top}
 .tg .tg-6kke{background-color:#efefef;color:#343434;vertical-align:top}
 .tg .tg-pikk{background-color:#efefef;color:#656565;vertical-align:top}
